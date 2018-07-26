9ec08e0da9d4bbc7133583ca949c6a3f28b9327b
Roman Khoma <cilen.ua@gmail.com>
2018-07-26 17:53:24 +0300

    додати авторизацію

---
+++ database/migrations/2018_07_24_121419_create_clocks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clocks', function (Blueprint $table) {
+            $table->increments('clock_id');
+            $table->string('name');
+            $table->string('gender');
+            $table->string('type_of_indexation');
+            $table->string('type_of_mechanism');
+            $table->string('producer');
+            $table->integer('price');
+            $table->integer('old_price');
+            $table->string('logo_uuid');
+            $table->timestamps();
+        });
+        DB::statement("ALTER TABLE clocks AUTO_INCREMENT = 1000;");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clocks');
+    }
+}
---
+++ database/migrations/2018_07_26_173230_create_clock_functions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClockFunctionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clock_functions', function (Blueprint $table) {
+            $table->increments('function_id');
+            $table->string('function_name');
+            $table->integer('clock_id')
+                  ->references('clock_id')->on('clocks')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->string('value_uk')->nullable();
+            $table->string('value_ru')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clock_functions');
+    }
+}
